9a3010f1e6810cd244378416f8cd8a6fd5ad3bdd
CTO <cto@paperclip.ing>
2026-05-15 07:20:05 +0800

    Render payment return confirmation route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 import App from './App';
 import AdminLogin from './admin/AdminLogin';
 import AdminDashboard from './admin/AdminDashboard';
+import BookingProcessing from './components/BookingProcessing';
 import PrintableTickets from './components/PrintableTickets';
 import Tutorial from './components/Tutorial';
 import './index.css';
+
+function PaymentReturnRoute() {
+  const { bookingId } = useParams();
+  return <BookingProcessing bookingId={bookingId || ''} />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/booking/:bookingId/processing" element={<PaymentReturnRoute />} />
+        <Route path="/booking/:bookingId/cancelled" element={<PaymentReturnRoute />} />
         <Route path="/tickets" element={<PrintableTickets />} />
         <Route path="/tickets/:ref" element={<PrintableTickets />} />
         <Route path="/tutorial" element={<Tutorial />} />
